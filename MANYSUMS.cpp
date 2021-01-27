29240fcbc4f7d2f6c4adc451c1c800a7788074ab
Divyanshu Kumar <38113387+divyanshukumar10@users.noreply.github.com>
2021-01-27 12:45:22 +0530

    Add files via upload

---
+++ MANYSUMS.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	long long int t;
+	cin>>t;
+	while(t--)
+	{
+	    long long int a,b;
+	    cin>>a>>b;
+	    
+	    cout<<abs(2*b - 2*a +1)<<endl;
+	}
+
+}
