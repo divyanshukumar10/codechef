fb69633a96ae3ffe62d3104c521b66e03663acdc
Divyanshu Kumar <38113387+divyanshukumar10@users.noreply.github.com>
2021-01-31 02:31:52 +0530

    Add files via upload

---
+++ SUMPOS.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    int n1, n2, n3;
+    cin >> n1 >> n2 >> n3;
+
+    if(n1 >= n2 && n1 >= n3 && n1==n2+n3)
+        cout << "YES" << endl;
+
+    else if(n2 >= n1 && n2 >= n3 && n2==n1+n3)
+        cout << "YES" << endl;
+    
+    else if(n3 >= n1 && n3 >= n2 && n3==n1+n2)
+        cout << "YES" << endl;
+        
+    else
+    cout << "NO" << endl;
+    }
+}
