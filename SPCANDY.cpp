6b45406beaeb51a0b4db98f4f9a5908bfedd777d
Divyanshu Kumar <38113387+divyanshukumar10@users.noreply.github.com>
2021-08-19 11:53:19 +0530

    Add files via upload

---
+++ SPCANDY.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int n,k;
+        cin>>n>>k;
+        
+        if(k==0)
+        {
+            cout<<"0 "<<n<<"\n";
+        }
+        else
+        cout<<n/k<<" "<<n%k<<"\n";
+        
+        
+        
+        
+        
+        
+    }
+	
+}
