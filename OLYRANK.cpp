eaf6f7f9401e5d35e84e27c796d60acd645e674b
Divyanshu Kumar <38113387+divyanshukumar10@users.noreply.github.com>
2021-08-22 12:41:00 +0530

    Add files via upload

---
+++ OLYRANK.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    long long int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int a,b,c,d,e,f;
+        cin>>a>>b>>c>>d>>e>>f;
+        long long int sum1=a+b+c;
+        long long int sum2=d+e+f;
+        if(sum1 > sum2 )
+        cout<<"1"<<endl;
+        else
+        cout<<"2"<<endl;
+        
+        
+    }
+	
+}
